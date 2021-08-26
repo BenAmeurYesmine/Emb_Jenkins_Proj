7aa396f89f773b0604213173e68b4ba7fd8ee5ac
BenAmeurYesmine <yesmine.benameur@insat.u-cathage.tn>
2021-08-26 13:15:43 +0100

    Doxygen script modified

--- TOOLS/doxygen.py
+++ TOOLS/doxygen.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-path  ="C:\\Users\\ASUS\\.jenkins\\workspace\\Emb_Jenkins_Proj_Debug\\TOOLS\\Doxygen"
+path  =".\\Doxygen"
 os.chdir(path)
 os.system("doxygen Doxyconfig")
 
