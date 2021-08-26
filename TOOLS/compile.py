06043423e3dd7760f54aa028d530e849caa79960
BenAmeurYesmine <yesmine.benameur@insat.u-cathage.tn>
2021-08-26 17:24:52 +0100

    global environment added to script

--- TOOLS/compile.py
+++ TOOLS/compile.py
@@ -10,6 +10,10 @@ build_target=sys.argv[1]
 #path  ="C:\\Users\\ASUS\\.jenkins\\workspace\\"+jenkins_job+"\\"+build_target
 path  ="..\\"+build_target
 os.chdir(path)
+
+os.environ["PATH"]+=os.pathsep + os.pathsep.join(["C:\\dev\\tools\\make-3.81\\bin"])
+os.environ["PATH"]+=os.pathsep + os.pathsep.join(["C:\\dev\\tools\\gcc-arm-none-eabi-10.3-2021.07\\bin"])
+
 os.system("make -j8 all")
 
 
