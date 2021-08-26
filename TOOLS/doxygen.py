343f3ed1ab93b1475c69d869f488c1d01299cb72
BenAmeurYesmine <yesmine.benameur@insat.u-cathage.tn>
2021-08-26 10:59:01 +0100

    Doxygen generarion script modified 2

--- TOOLS/doxygen.py
+++ TOOLS/doxygen.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-path  ="C:\\Users\\ASUS\\.jenkins\\workspace\\Emb_Jenkins_Proj_Debug\\TOOLS"
+path  ="C:\\Users\\ASUS\\.jenkins\\workspace\\Emb_Jenkins_Proj_Debug\\TOOLS\\Doxygen"
 os.chdir(path)
 os.system("doxygen Doxyconfig")
 
